6dffe7df0d70ab623f70c34af8be93add4114a6a
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2024-07-12 21:37:38 +0530

    completed card part and added box shadow

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -69,12 +69,13 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     } else {
       var currScale = 0.8;
       matrix = Matrix4.diagonal3Values(1, currScale, 1)
-        ..setTranslationRaw(0, _height * (1 - _scaleFactor)/2, 1);
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
     }
     return Transform(
       transform: matrix,
       child: Stack(
         children: [
+          // main outer container
           Container(
             height: 220,
             margin: const EdgeInsets.only(left: 10, right: 10),
@@ -88,21 +89,37 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                   image: AssetImage("assets/image/food0.png")),
             ),
           ),
+
+          // smaller container
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
               height: 120,
               margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-              ),
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0xFFe8e8e8),
+                      blurRadius: 5.0,
+                      offset: Offset(0, 5),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-5, 0),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(5, 0),
+                    )
+                  ]),
               child: Container(
                 padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    BigText(text: "Chinese Side"),
+                    const BigText(text: "Chinese Side"),
                     const SizedBox(height: 10),
                     Row(
                       children: [
